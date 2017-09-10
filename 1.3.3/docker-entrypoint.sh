f0bd0cdff829965d09a085ce832fa09ed3ee5e2b
Peter Grman <peter.grman@gmail.com>
2017-09-10 15:50:00 +0200

    fix admin installation

--- 1.3.3/docker-entrypoint.sh
+++ 1.3.3/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ -z "$(ls -A /var/www/html)"]; then
 	cp -a /usr/src/grav/. /var/www/html/
-	/var/www/html/bin/gpm install admin
+	/var/www/html/bin/gpm install -y admin
 	if [ "$GRAV_REVERSE_PROXY" = "true" ]; then
 		sed -i "s/reverse_proxy_setup:\sfalse/reverse_proxy_setup: true/g" \
 		        system/config/system.yaml
